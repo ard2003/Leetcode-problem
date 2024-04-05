5ded2e1d4584f22652d39207dfc3fdff1782036c
arshad <arshadshibu45@gmail.com>
2024-04-05 10:08:26 +0530

    first commit

---
+++ 2665. Counter II.js
@@ -0,0 +1,10 @@
+var createCounter = function(init) {
+    let presentCount=init;
+    
+
+    return {
+        increment:()=> ++presentCount,
+        decrement:()=> --presentCount,
+        reset:()=> presentCount = init,
+    }
+};
